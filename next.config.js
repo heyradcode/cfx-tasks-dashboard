d44ae4785fdb6ed7f8f3e72393df802153172195
heyradcode <betterthanbestest@gmail.com>
2025-09-25 18:25:55 -0700

    feat: upgrade deps

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   compress: true,
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ['@headlessui/react', 'chart.js', 'react-chartjs-2']
+  }
 }
 
 module.exports = nextConfig
